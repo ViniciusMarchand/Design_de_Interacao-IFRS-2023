343346c1ccca213a1d26dba360c8f3b4782bbfdc
Vinicius Marchand <vinicius.dasilva.marchand@gmail.com>
2023-08-23 00:36:17 -0300

    att

--- Trabalho1/script.js
+++ Trabalho1/script.js
@@ -1,8 +1,8 @@
 
 let jogoConfirmado = false;
-const FORM = document.getElementById('form')
-let corFinalEsquerdo;
-let corFinalDireito;
+const FORM = document.getElementById('form');
+let corEsquerda;
+let corDireita;
 FORM.addEventListener('submit', e => {
     e.preventDefault()
     let validado = true
@@ -29,13 +29,15 @@ function comecaJogo() {
     const time4 = document.getElementById('time4');
 
     const AREA_JOGO = document.getElementById('area-jogo');
-    AREA_JOGO.innerHTML = `  <div class="semi-final esquerda" id="semi-final1">
+    AREA_JOGO.innerHTML = `
+    <input type="button" value="CONFIRMAR" onclick="resultadoSemiFinalVerificacao('1','2')" id="confimar-mobile-cima">       
+<div class="semi-final esquerda" id="semi-final1">
     <div class="label-input-jogo" id='jogo1-time1'>
         <input type="number" class="input-number" placeholder="Gols" id='time1-semi-final'>
         <label for="" id="label-semi-time1">${time1.value}</label>
     </div>
-    <input type="button" value="CONFIRMAR" onclick="resultadoSemiFinalVerificacao('1','2')" id="confimar-esquerdo">                
-    <div class="label-input-jogo" id='jogo1-time2'>
+    <input type="button" value="CONFIRMAR" onclick="resultadoSemiFinalVerificacao('1','2')" id="confimar-esquerdo-desktop">                
+    <div class="label-input-jogo mobile-position" id='jogo1-time2'>
         <label for="" id="label-semi-time2">${time2.value}</label>
         <input type="number" class="input-number" placeholder="Gols" id='time2-semi-final'>
     </div>            
@@ -52,16 +54,17 @@ function comecaJogo() {
     </div>
 </div>
 <div class="semi-final direita" id="semi-final2">
-    <div class="label-input-jogo" id='jogo1-time3'>
+    <div class="label-input-jogo mobile-position" id='jogo1-time3'>
         <input type="number" class="input-number" placeholder="Gols" id='time3-semi-final'>
         <label for="" id="label-semi-time3">${time3.value}</label>
     </div>
-    <input type="button" value="CONFIRMAR" id="confimar-direito" onclick="resultadoSemiFinalVerificacao('3','4')">                
+    <input type="button" value="CONFIRMAR" id="confimar-direito-desktop" onclick="resultadoSemiFinalVerificacao('3','4')">                
     <div class="label-input-jogo" id='jogo1-time4'>
         <label for="" id="label-semi-time4">${time4.value}</label>
         <input type="number" class="input-number" placeholder="Gols" id='time4-semi-final'>
     </div>
-</div>`
+</div>
+<input type="button" value="CONFIRMAR" onclick="resultadoSemiFinalVerificacao('3','4')" id="confimar-mobile-baixo">  `
 
     for (let i = 1; i <= 4; i++) {
         const elementoCor = document.getElementById(`time${i}cor`).value;
@@ -78,6 +81,7 @@ function colorirDesabilitar() {
         const ELEMENTO = document.getElementById(`label${i}`);
         const VALOR_INPUT = document.getElementById(`time${i}`);
         const BOTAO_CONFIRMAR = document.getElementById('botao-confirmar');
+
         ELEMENTO.style.border = `5px solid`;
         ELEMENTO.style.borderColor = `${INPUT_COR.value}`;
         VALOR_INPUT.setAttribute('disabled', '');
@@ -101,40 +105,58 @@ function resultadoSemiFinalVerificacao(num1, num2) {
 function resultadoSemiFinal(num1, num2) {
     const INPUT_TIME1 = document.getElementById(`time${num1}-semi-final`);
     const INPUT_TIME2 = document.getElementById(`time${num2}-semi-final`);
-    let confirmarEsquerdo;
-    if(num1 == 1){
-        confirmarEsquerdo = document.getElementById('confimar-esquerdo');
-    }else{
-        confirmarEsquerdo = document.getElementById('confimar-direito');
-
+    let confirmarBotao;
+    if (num1 == 1) {
+        confirmarBotao = document.getElementById('confimar-esquerdo-desktop');
+    } else {
+        confirmarBotao = document.getElementById('confimar-direito-desktop');
     }
-    INPUT_TIME1.setAttribute('disabled', '');
-    INPUT_TIME2.setAttribute('disabled', '');
-    confirmarEsquerdo.setAttribute('disabled', '');
-
+    if (num1 == 1) {
+        confirmarBotao = document.getElementById('confimar-mobile-cima');
+    } else {
+        confirmarBotao = document.getElementById('confimar-mobile-baixo');
+    }
+    let empate = false;
     const golsTime1 = document.getElementById(`time${num1}-semi-final`).value;
     const golsTime2 = document.getElementById(`time${num2}-semi-final`).value;
     let ganhador;
     if (parseInt(golsTime1) > parseInt(golsTime2)) {
         const COR_TIME1 = document.getElementById(`time${num1}cor`).value;
-        corFinalDireito = COR_TIME1
         const JOGO1 = document.getElementById(`jogo1-time${num1}`);
         JOGO1.style.background = `${COR_TIME1}`;
+        if(num1 == '1'){
+            corEsquerda = COR_TIME1;
+        }else{
+            corDireita = COR_TIME1
+        }
         ganhador = document.getElementById(`label-semi-time${num1}`).innerText;
     } else if (parseInt(golsTime2) > parseInt(golsTime1)) {
         const COR_TIME2 = document.getElementById(`time${num2}cor`).value;
-        corFinalEsquerdo = COR_TIME2; 
         const JOGO2 = document.getElementById(`jogo1-time${num2}`);
         JOGO2.style.background = `${COR_TIME2}`;
         ganhador = document.getElementById(`label-semi-time${num2}`).innerText;
+        if(num1 == '1'){
+            corEsquerda = COR_TIME2;
+        }else{
+            corDireita = COR_TIME2
+        }
+    } else {
+        deuEmpate();
+        empate = true;
     }
 
-    if (num1 == '1') {
-        document.getElementById('label-final1').innerText = ganhador;
-        document.getElementById('time5-final').removeAttribute('disabled', '');
-    } else {
-        document.getElementById('label-final2').innerText = ganhador;
-        document.getElementById('time6-final').removeAttribute('disabled', '');
+    if (!empate) {
+        INPUT_TIME1.setAttribute('disabled', '');
+        INPUT_TIME2.setAttribute('disabled', '');
+        confirmarBotao.setAttribute('disabled', '');
+        if (num1 == '1') {
+            document.getElementById('label-final1').innerText = ganhador;
+            document.getElementById('time5-final').removeAttribute('disabled', '');
+        } else {
+            document.getElementById('label-final2').innerText = ganhador;
+            document.getElementById('time6-final').removeAttribute('disabled', '');
+
+        }
 
     }
 }
@@ -157,25 +179,43 @@ function resultadoFinal() {
     const INPUT_TIME1 = document.getElementById(`time5-final`);
     const INPUT_TIME2 = document.getElementById(`time6-final`);
     const CONFIRMAR_ESQUERDO = document.getElementById('confimar-esquerdo');
-    INPUT_TIME1.setAttribute('disabled', '');
-    INPUT_TIME2.setAttribute('disabled', '');
-    CONFIRMAR_ESQUERDO.setAttribute('disabled', '');
-
     const golsTime1 = document.getElementById(`time5-final`).value;
     const golsTime2 = document.getElementById(`time6-final`).value;
     let ganhador;
+    let empate = false;
     if (parseInt(golsTime1) > parseInt(golsTime2)) {
         const JOGO1 = document.getElementById(`final1-block`);
-        JOGO1.style.background = `${corFinalEsquerdo}`;
-        ganhador = document.getElementById('time5-final').innerText;
-        document.body.style.backgroundColor = corFinalDireito;
-        
-
+        ganhador = document.getElementById('label-final1').innerText;
+        document.body.style.backgroundColor = corEsquerda;
+        JOGO1.style.backgroundColor = corEsquerda;
+        document.getElementById('pagina-titulo-block').innerHTML=`<h2>${ganhador} Venceu!</h2>`;
     } else if (parseInt(golsTime2) > parseInt(golsTime1)) {
         const JOGO2 = document.getElementById('final2-block');
-        JOGO2.style.background = `${corFinalDireito}`;
-        ganhador = document.getElementById('time6-final').innerText;
+        ganhador = document.getElementById('label-final2').innerText;
+        document.body.style.backgroundColor = corDireita;
+        JOGO2.style.backgroundColor = corDireita;
+        document.getElementById('pagina-titulo-block').innerHTML=`<h2>${ganhador} Venceu!</h2>`;
+
+    } else {
+        deuEmpate();
+        empate = true;
     }
 
+}
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+    alertPlaceholder.append(wrapper)
+}
+
+function deuEmpate() {
+    appendAlert('Adicione o resultado dos pênaltis', 'success')
 }
 
